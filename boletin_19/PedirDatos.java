41a91d0431e21affd3b0a449b448f6d0cb289296
afernandezfontenla <56158212+afernandezfontenla@users.noreply.github.com>
2020-02-28 20:30:56 +0100

    Add files via upload

---
+++ boletin_19/PedirDatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_19;
+
+/**
+ *
+ * @author Nuevo usuario
+ */
+public class PedirDatos {
+
+    static int pedirInt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String pedirMensaxe(String nome_filas_) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
